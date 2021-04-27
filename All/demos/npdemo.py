d938026b4e97484cec9942a1ab66ea275e7c6d14
acsstudios <acsstudiosdev@gmail.com>
2021-04-27 21:15:27 +0200

    stack

---
+++ All/demos/npdemo.py
@@ -0,0 +1,6 @@
+#https://stackoverflow.com/questions/51216579/how-to-combine-two-columsn-rows-of-a-matrix-in-numpy-by-summing-values
+import numpy as np
+
+a = np.ones((3,2))        # a 2D array with 3 rows, 2 columns, filled with ones
+b = np.array([1,2,3])     # a 1D array initialised using a list [1,2,3]
+c = np.linspace(2,3,100)  # an array with 100 points beteen (and including) 2 and 3
